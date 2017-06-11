d9da49a6852e8fe75f4a596b880a5ed8288af727
EDeoMa <edeoma@gmail.com>
2017-06-11 22:58:51 +0300

    Поправил

--- app/src/test/java/com/dbkudryavtsev/childrencrosswords/utilities/CrosswordIOJSONTest.java
+++ app/src/test/java/com/dbkudryavtsev/childrencrosswords/utilities/CrosswordIOJSONTest.java
@@ -7,22 +7,56 @@ import org.mockito.runners.MockitoJUnitRunner;
 import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
-public class CrosswordIOJSONTest { //TODO VK  // не хватает тестов на краевые случаи. Что будет если подать пустой массив? null? некорректный json?
+public class CrosswordIOJSONTest {
     @Test
-    public void parseAnswersFromJson() throws Exception {
-        String jsonString = "{ \"answers\": [{\n" +
-                "        \"answer\": \"РАСЧЁСКА\"},\n" +
-                "        {\n" +
-                "        \"answer\": \"ВОРОБЕЙ\"},\n" +
-                "        {\n" +
-                "        \"answer\": \"РОБОТ\"},\n" +
-                "        {\n" +
-                "        \"answer\": \"СОРОКА\"}] }";
+    public void parseAnswersFromJson(){
+        String jsonString = "{ \"answers\": [" +
+                "{\n \"answer\": \"РАСЧЁСКА\"},\n" +
+                "{\n \"answer\": \"ВОРОБЕЙ\"},\n" +
+                "{\n \"answer\": \"РОБОТ\"},\n" +
+                "{\n \"answer\": \"СОРОКА\"}" +
+                "] }";
+
+        String emptyJSON = "{\"answers\": []}";
+
+        String nullJSON = null;
+
+        String wrongJSON = "{ \"answers\": {" +
+                "{\n \"answer\": \"РАСЧЁСКА\"},\n" +
+                "{\n \"answer\": \"ВОРОБЕЙ\"},\n" +
+                "{\n \"answer\": \"РОБОТ\"},\n" +
+                "{\n \"answer\": \"СОРОКА\"}" +
+                "] }";
 
         String[] rightAnswers = new String[]{"РАСЧЁСКА", "ВОРОБЕЙ", "РОБОТ", "СОРОКА"};
 
+        String[] emptyAnswers = new String[0];
+
+        String[] nullAnswers = null;
+
+        boolean isThrown = false;
+
         String[] result = CrosswordIOJSON.parseAnswersFromJson(jsonString);
 
+        String[] emptyResult = CrosswordIOJSON.parseAnswersFromJson(emptyJSON);
+
+        String[] nullResult = CrosswordIOJSON.parseAnswersFromJson(nullJSON);
+
         assertEquals(rightAnswers, result);
+
+        assertEquals(emptyAnswers, emptyResult);
+
+        assertEquals(nullAnswers, nullResult);
+
+        try {
+            CrosswordIOJSON.parseAnswersFromJson(wrongJSON);
+        }
+        catch (RuntimeException ex){
+            isThrown = true;
+        }
+        finally {
+            assertTrue(isThrown);
+        }
+
     }
 }
